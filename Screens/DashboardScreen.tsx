ea03a211b337a2fb7f0ae9f5972898bbe88eff72
kmadhav907 <kmadhav907@gmail.com>
2024-01-24 12:55:16 +0530

    Geolocation and Modal setup

--- Screens/DashboardScreen.tsx
+++ Screens/DashboardScreen.tsx
@@ -1,47 +1,137 @@
-import React, {useState} from 'react';
-import {StyleSheet, Switch, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import Modal from 'react-native-modal';
 import globalStyles from '../Styles/globalStyles';
 import Map from '../Component/Map';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {ParamListBase} from '@react-navigation/native';
-import {black, white} from '../Constants/ColorScheme';
+import {black, white, yellow} from '../Constants/ColorScheme';
 import {HEIGHT, WIDTH} from '../Constants/Dimensions';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import ToggleSwitch from 'toggle-switch-react-native';
+import {requestLocationPermission} from '../Utils/requests';
+import {Cordinates} from '../Types/LocationTypes';
+import Geolocation, {
+  GeolocationResponse,
+} from '@react-native-community/geolocation';
 
 interface DashboardScreenProps {
   navigation: StackNavigationProp<ParamListBase, string>;
 }
 
 const DashboardScreen = (props: DashboardScreenProps) => {
+  const [currentCords, setCurrentCords] = useState<Cordinates | null>(null);
   const [userStatus, setUserStatus] = useState<boolean>(true);
+  const [modalDrawer, setModalDrawer] = useState<boolean>(false);
+
+  useEffect(() => {
+    let watchId: number;
+    requestLocationPermission().then(response => {
+      if (response) {
+        Geolocation.getCurrentPosition(
+          (positon: GeolocationResponse) => {
+            setCurrentCords({
+              latitude: positon.coords.latitude,
+              longitude: positon.coords.longitude,
+            });
+          },
+          err => {
+            console.log(err);
+          },
+          {
+            enableHighAccuracy: true,
+            timeout: 30000,
+            maximumAge: 1000,
+          },
+        );
+        watchId = Geolocation.watchPosition(
+          (position: GeolocationResponse) => {
+            setCurrentCords({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            });
+          },
+          err => {
+            console.log(err);
+          },
+          {
+            enableHighAccuracy: true,
+            maximumAge: 1000,
+          },
+        );
+      }
+    });
+    return () => {
+      Geolocation.clearWatch(watchId);
+    };
+  }, []);
+  const handleOpenModal = () => {
+    setModalDrawer(true);
+  };
+  const toggleDrawer = () => {
+    setModalDrawer(false);
+  };
   return (
-    <View style={globalStyles.screenContainer}>
-      <View style={dashboardStyles.iconContainer}>
-        <Icon name="align-justify" color={black} size={24} />
-        <ToggleSwitch
-          isOn={userStatus}
-          onColor={white}
-          offColor={black}
-          thumbOnStyle={{backgroundColor: black}}
-          onToggle={status => {
-            setUserStatus(status);
-          }}
-          size="medium"></ToggleSwitch>
-      </View>
-      <View
-        style={[globalStyles.screenSection, dashboardStyles.dashboardContent]}>
-        <View style={dashboardStyles.headerSection}>
-          <Icon name="circle" color="green" />
-          <Text style={dashboardStyles.headerTitle}>Place Name</Text>
+    <>
+      {!currentCords ? (
+        <View>
+          <Text>Need Location Access</Text>
         </View>
-        <Map
-          latitude={37.78825}
-          longitude={-122.4324}
-          navigation={props.navigation}
-        />
-      </View>
-    </View>
+      ) : (
+        <View style={globalStyles.screenContainer}>
+          {modalDrawer && (
+            <Modal
+              isVisible={modalDrawer}
+              animationIn="slideInLeft"
+              animationOut="slideOutLeft"
+              onBackButtonPress={toggleDrawer}
+              style={dashboardStyles.modalStyle}
+              hideModalContentWhileAnimating
+              onBackdropPress={toggleDrawer}
+              useNativeDriver
+              swipeDirection="left">
+              <SafeAreaView></SafeAreaView>
+            </Modal>
+          )}
+          <View style={dashboardStyles.iconContainer}>
+            <TouchableOpacity onPress={handleOpenModal}>
+              <Icon name="align-justify" color={black} size={24} />
+            </TouchableOpacity>
+
+            <ToggleSwitch
+              isOn={userStatus}
+              onColor={white}
+              offColor={black}
+              thumbOnStyle={{backgroundColor: black}}
+              onToggle={status => {
+                setUserStatus(status);
+              }}
+              size="medium"></ToggleSwitch>
+          </View>
+          <View
+            style={[
+              globalStyles.screenSection,
+              dashboardStyles.dashboardContent,
+            ]}>
+            <View style={dashboardStyles.headerSection}>
+              <Icon name="circle" color="green" />
+              <Text style={dashboardStyles.headerTitle}>Place Name</Text>
+            </View>
+            <Map
+              latitude={currentCords.latitude}
+              longitude={currentCords.longitude}
+              navigation={props.navigation}
+            />
+          </View>
+        </View>
+      )}
+    </>
   );
 };
 
@@ -76,6 +166,16 @@ const dashboardStyles = StyleSheet.create({
     alignItems: 'center',
     padding: 10,
     width: WIDTH,
+  },
+  modalStyle: {
+    width: WIDTH * 0.75,
+    // backgroundColor: "#f9d342",
+    backgroundColor: 'black',
+    color: 'white',
+    margin: 0,
+    flex: 1,
+    borderTopRightRadius: 15,
+    // borderBottomRightRadius: 15,
   },
 });
 export default DashboardScreen;
